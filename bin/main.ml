0e5d892d2a1fff75261fe9a66232104d3da8caea
Can Ersoy <can.ersoy@tum.de>
2023-09-11 11:31:07 +0300

    .

--- bin/main.ml
+++ bin/main.ml
@@ -13,6 +13,29 @@ let rec at list k = match list with
 | x::xy -> if k = 0 then Some x else at xy (k-1)
 | [] -> None
 
+let length list = 
+  let rec counter list k = match list with
+  | [] -> k
+  | _::xy -> counter xy (k+1)
+  in
+  counter list 0 
+
+let rec rev list = match list with
+| x::xy -> rev xy @ [x]
+| [] -> []
+
+let palindrome list = if rev list == list then true else false
+
+type 'a node =
+    | One of 'a 
+    | Many of 'a node list
+
+let rec flatten list = match list with
+| One a::xy -> [a] @ flatten xy
+| Many a::xy -> flatten a @ flatten xy  
+| [] -> [] 
+
+
 
 
 let () = 
